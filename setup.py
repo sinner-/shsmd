1fefa8eb0eacec9d838cdad1941dabefeff6fc4e
Sina Sadeghi <sina.sa@gmail.com>
2016-03-26 15:55:16 +1100

    add travisCI functionality

--- setup.py
+++ setup.py
@@ -2,6 +2,19 @@
 """
 
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
+import sys
+
+class Tox(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import tox
+        errcode = tox.cmdline(self.test_args)
+        sys.exit(errcode)
 
 setup(
     name='shsmd',
@@ -33,4 +46,6 @@ setup(
         'console_scripts': [
             'shsmd = shsmd.__main__:main'
         ]},
+    tests_require=['tox'],
+    cmdclass = {'test': Tox},
 )
